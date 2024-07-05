cc40b8c1f483dcbda6f1fce44245b3c2bcc322cc
Helô <heloisacmafra@gmail.com>
2024-07-05 15:58:49 +1200

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useRef, useState } from "react"
+import { Alert, View, Keyboard } from "react-native"
+import Bottom from "@gorhom/bottom-sheet"
+import { router } from "expo-router"
+import dayjs from "dayjs"
+import { Input } from "@/components/Input"
+import { Header } from "@/components/Header"
+import { Button } from "@/components/Button"
+import { BottomSheet } from "@/components/BottomSheet"
+import { Goals, GoalsProps } from "@/components/Goals"
+import { Transactions, TransactionsProps } from "@/components/Transactions"
+import { mocks } from "@/utils/mocks"
+
+export default function Home() {
+  const [transactions, setTransactions] = useState<TransactionsProps>([])
+  const [goals, setGoals] = useState<GoalsProps>([])
+
+  const [name, setName] = useState("")
+  const [total, setTotal] = useState("")
+
+  const bottomSheetRef = useRef<Bottom>(null)
+  const handleBottomSheetOpen = () => bottomSheetRef.current?.expand()
+  const handleBottomSheetClose = () => bottomSheetRef.current?.snapToIndex(0)
+
+  function handleDetails(id: string) {
+    router.navigate("/details/" + id)
+  }
+
+  async function handleCreate() {
+    try {
+      const totalAsNumber = Number(total.toString().replace(",", "."))
+
+      if (isNaN(totalAsNumber)) {
+        return Alert.alert("Error", "Invalid value.")
+      }
+
+      console.log({ name, total: totalAsNumber })
+
+      Keyboard.dismiss()
+      handleBottomSheetClose()
+      Alert.alert("Success", "Goal registered!")
+
+      setName("")
+      setTotal("")
+    } catch (error) {
+      Alert.alert("Error", "Unable to register.")
+      console.log(error)
+    }
+  }
+
+  async function fetchGoals() {
+    try {
+      const response = mocks.goals
+      setGoals(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function fetchTransactions() {
+    try {
+      const response = mocks.transactions
+
+      setTransactions(
+        response.map((item) => ({
+          ...item,
+          date: dayjs(item.created_at).format("DD/MM/YYYY [at] HH:mm"),
+        }))
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchGoals()
+    fetchTransactions()
+  }, [])
+
+  return (
+    <View className="flex-1 p-8">
+      <Header
+        title="myGoals"
+        subtitle="Save today to reap the rewards tomorrow."
+      />
+
+      <Goals
+        goals={goals}
+        onAdd={handleBottomSheetOpen}
+        onPress={handleDetails}
+      />
+
+      <Transactions transactions={transactions} />
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        title="New goal"
+        //snapPoints [high when closed, when open]
+        snapPoints={[0.01, 284]}
+        onClose={handleBottomSheetClose}
+      >
+        <Input placeholder="Goal" onChangeText={setName} value={name} />
+
+        <Input
+          placeholder="Value to save"
+          keyboardType="numeric"
+          onChangeText={setTotal}
+          value={total}
+        />
+
+        <Button title="Create" onPress={handleCreate} />
+      </BottomSheet>
+    </View>
+  )
+}
